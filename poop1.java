84cde876136632f6e2eedfb81094f3e65437e716
joshconfrio <143226219+joshconfrio@users.noreply.github.com>
2025-08-27 22:01:35 -0600

    Create poop1.java
    hola mundo junto con primeras sumas

---
+++ poop1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author poo02alu19
+ */
+public class PooClass {
+    
+    public static void main(String[] args){
+        System.out.println("Hola Mundo!");
+        
+        int a=3;
+        float b = 3.5f;
+        float c = a+b;
+        System.out.println(c);
+        double e = 7.4;
+        System.out.println(e);
+    }
+}
+
