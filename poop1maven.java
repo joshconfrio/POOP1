b911b6e50158d5310d1014e357cd3d7f045f3993
joshconfrio <143226219+joshconfrio@users.noreply.github.com>
2025-08-27 22:02:44 -0600

    Create poop1maven.java
    primer movimiento en netbeans con maven en java

---
+++ poop1maven.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven;
+
+/**
+ *
+ * @author poo02alu19
+ */
+public class POOP1_maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
